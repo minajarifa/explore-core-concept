543f23b8955bcadb6d5dbc4683ff64c1b807fc39
Your Name <minajmiha02@gmail.com>
2026-02-24 08:17:29 +0600

    added handle function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function handleClink() {
+    alert("I am Clicked");
+  }
+  const handleClickMe3=()=>{
+    alert(" I Am Click Me 3")
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h3>Vite + React</h3>
+        <button onClick={handleClink}>Click me</button>
+        <button
+          onClick={function handleClink2() {
+            alert("I am clicked 2");
+          }}
+        >
+          Click me 2
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={handleClickMe3}>clik me 3</button>
+        <button onClick={()=>alert("I am click")}>clik me 4</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
