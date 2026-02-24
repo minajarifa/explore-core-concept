750732b4bce6b268c2262a58cbf924fc68f26fcc
Your Name <minajmiha02@gmail.com>
2026-02-24 16:48:59 +0600

    end handle

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [runs, setRuns] = useState(0);
   const [sixes, setsixes] = useState(0);
-  const handleAdd=()=>{
-    const newCount= count+1
-    setCount(newCount)
-  }
-  const handleSix=()=>{
-    const updateRun= count+6
-    const updateSixs= sixes+1
-    setsixes(updateSixs)
-    set(updateSixs)
-  }
-  
+  const [fours, setFours] = useState(0);
+  const handleAdd = () => {
+    const newRuns = runs + 1;
+    console.log(runs);
+    setRuns(newRuns);
+  };
+  const handleSix = () => {
+    const updateRun = runs + 6;
+    setRuns(updateRun);
+    const updateSixs = sixes + 1;
+    setsixes(updateSixs);
+  };
+
+  const handleFour = () => {
+    const updateRuns = runs + 4;
+    setRuns(updateRuns);
+    const updateRour = fours + 1;
+    setFours(updateRour);
+  };
+
   const counterStyle = {
     border: "2px solid yellow",
     margin: "30px",
   };
   return (
-    <div style={counterStyle}>
-      <h3>Count: {count}</h3>
-      <button onClick={handleAdd}>singles</button>
-      <button >Four</button>
-      <button >six</button>
+    <div>
+      <p>six: {sixes}</p>
+      <p>four: {fours}</p>
+      <div style={counterStyle}>
+        <p>Runs: {runs}</p>
+        <button onClick={handleAdd}>singles</button>
+        <button onClick={handleSix}>six</button>
+        <button onClick={handleFour}>Four</button>
+      </div>
     </div>
   );
 }
