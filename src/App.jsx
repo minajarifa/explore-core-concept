a5bd6f3ef7020923f36827c7c1d2c8b3925a2c4e
Your Name <minajmiha02@gmail.com>
2026-02-24 17:37:42 +0600

    added nasted route and heaader or footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import Counter from "./Counter";
+import Header from "./Component/Header";
+import Footer from "./Component/Footer";
 
 function App() {
-  function handleClink() {
-    alert("I am Clicked");
-  }
-  const handleClickMe3 = () => {
-    alert(" I Am Click Me 3");
-  };
-  const handleAdd = (num) => {
-    const newNum = num + 1;
-    alert(newNum);
-  };
   return (
     <>
       <div>
-        <h3>Vite + React</h3>
-        <Counter/>
-        <button onClick={handleClink}>Click me</button>
-        <button
-          onClick={function handleClink2() {
-            alert("I am clicked 2");
-          }}
-        >
-          Click me 2
-        </button>
-        <button onClick={handleClickMe3}>clik me 3</button>
-        <button onClick={() => alert("I am click")}>clik me 4</button>
-        <button onClick={() => handleAdd(1)}>Click me 5</button>
+        <Header />
+        <Outlet />
+        <Footer />
       </div>
     </>
   );
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,11 +4,11 @@ export default function Counter() {
   const [runs, setRuns] = useState(0);
   const [sixes, setsixes] = useState(0);
   const [fours, setFours] = useState(0);
-  const handleAdd = () => {
-    const newRuns = runs + 1;
-    console.log(runs);
-    setRuns(newRuns);
-  };
+  // const handleAdd = () => {
+  //   const newRuns = runs + 1;
+  //   console.log(runs);
+  //   setRuns(newRuns);
+  // };
   const handleSix = () => {
     const updateRun = runs + 6;
     setRuns(updateRun);
@@ -27,6 +27,22 @@ export default function Counter() {
     border: "2px solid yellow",
     margin: "30px",
   };
+  function handleClink() {
+    alert("I am Clicked");
+  }
+  const handleClickMe3 = () => {
+    alert(" I Am Click Me 3");
+  };
+ 
+  fetch(`https://jsonplaceholder.typicode.com/posts`)
+  .then(res=>res.json())
+  .then(data=>console.log(data))
+  // const loadData=async()=>{
+  //   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  //   const data = res.json()
+  //   console.log(data) 
+
+  // }
   return (
     <div>
       <p>six: {sixes}</p>
@@ -36,6 +52,21 @@ export default function Counter() {
         <button onClick={handleAdd}>singles</button>
         <button onClick={handleSix}>six</button>
         <button onClick={handleFour}>Four</button>
+      </div>
+        <div>
+        <h3>Vite + React</h3>
+       
+        <button onClick={handleClink}>Click me</button>
+        <button
+          onClick={function handleClink2() {
+            alert("I am clicked 2");
+          }}
+        >
+          Click me 2
+        </button>
+        <button onClick={handleClickMe3}>clik me 3</button>
+        <button onClick={() => alert("I am click")}>clik me 4</button>
+        <button onClick={() => handleAdd(1)}>Click me 5</button>
       </div>
     </div>
   );
